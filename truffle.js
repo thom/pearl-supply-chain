2b39ab5d988ca474fe39bf8438bde8b88a0913fd
Thomas Weibel <thom@ikhono.net>
2019-05-18 19:01:09 +0200

    Add pragma "^0.4.24" for Solidity

--- truffle.js
+++ truffle.js
@@ -1,4 +1,10 @@
 module.exports = {
+  compilers: {
+    solc: {
+      version: "^0.4.24", // A version or constraint - Ex. "^0.5.0"
+                          // Can also be set to "native" to use a native solc
+    }
+  },
   networks: {
     development: {
       host: "127.0.0.1",
