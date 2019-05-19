ceac36ff2fd3e246e4ab81ed3538f776ca8307ca
Thomas Weibel <thom@ikhono.net>
2019-05-19 18:15:31 +0200

    Prepare for contract deployment to the Rinkeby test network

--- truffle.js
+++ truffle.js
@@ -1,3 +1,9 @@
+const fs = require('fs');
+
+const HDWalletProvider = require('truffle-hdwallet-provider');
+const infuraKey = fs.readFileSync(".infura-key").toString().trim();
+const mnemonic = fs.readFileSync(".mnemonic").toString().trim();
+
 module.exports = {
   compilers: {
     solc: {
@@ -10,6 +16,12 @@ module.exports = {
       host: "127.0.0.1",
       port: 8545,
       network_id: "*" // Match any network id
+    },
+    rinkeby: {
+      provider: () => new HDWalletProvider(mnemonic, `https://rinkeby.infura.io/v3/${infuraKey}`),
+        network_id: 4,       // rinkeby's id
+        gas: 4500000,        // rinkeby has a lower block limit than mainnet
+        gasPrice: 10000000000
     }
   }
 };
