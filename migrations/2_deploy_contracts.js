233fc6413026959aed95fadf32f80e94f57733ab
Thomas Weibel <thom@ikhono.net>
2019-05-19 14:56:32 +0200

    Update with correct roles

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,12 +1,12 @@
 // migrating the appropriate contracts
-var FarmerRole = artifacts.require("./FarmerRole.sol");
+var PearlFarmerRole = artifacts.require("./PearlFarmerRole.sol");
 var DistributorRole = artifacts.require("./DistributorRole.sol");
 var RetailerRole = artifacts.require("./RetailerRole.sol");
 var ConsumerRole = artifacts.require("./ConsumerRole.sol");
 var SupplyChain = artifacts.require("./SupplyChain.sol");
 
 module.exports = function(deployer) {
-  deployer.deploy(FarmerRole);
+  deployer.deploy(PearlFarmerRole);
   deployer.deploy(DistributorRole);
   deployer.deploy(RetailerRole);
   deployer.deploy(ConsumerRole);
