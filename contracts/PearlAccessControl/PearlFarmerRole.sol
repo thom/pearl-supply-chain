d2d65cc681c4084ee1af2f233191293fe577571e
Thomas Weibel <thom@ikhono.net>
2019-05-19 13:11:13 +0200

    Add error message to onlyFarmer modifier

--- contracts/PearlAccessControl/PearlFarmerRole.sol
+++ contracts/PearlAccessControl/PearlFarmerRole.sol
@@ -21,7 +21,7 @@ contract PearlFarmerRole {
 
   // Define a modifier that checks to see if msg.sender has the appropriate role
   modifier onlyFarmer() {
-    require(isFarmer(msg.sender));
+    require(isFarmer(msg.sender), "Caller is not a pearl farmer.");
     _;
   }
 
